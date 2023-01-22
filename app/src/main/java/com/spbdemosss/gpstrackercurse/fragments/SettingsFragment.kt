22fad856848dda2353a53774b849a53ae8f369ca
demosss <spbdemosss@gmail.com>
2023-01-22 17:26:56 +0300

    lesson_6

--- app/src/main/java/com/spbdemosss/gpstrackercurse/fragments/SettingsFragment.kt
+++ app/src/main/java/com/spbdemosss/gpstrackercurse/fragments/SettingsFragment.kt
@@ -1,11 +1,34 @@
 package com.spbdemosss.gpstrackercurse.fragments
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.spbdemosss.gpstrackercurse.R
+import com.spbdemosss.gpstrackercurse.utils.showToast
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    private lateinit var timePref: Preference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
+        init()
+    }
+
+    private fun init(){
+        timePref = findPreference("update_time_key")!!
+        val changeListener = onChangeListener()
+        timePref.onPreferenceChangeListener = changeListener
+    }
+
+    private fun onChangeListener(): Preference.OnPreferenceChangeListener{
+        return Preference.OnPreferenceChangeListener{
+            pref, value ->
+            val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+            val valueArray = resources.getStringArray(R.array.loc_time_update_value)
+            val title = pref.title.toString().substringBefore(":")
+            pref.title = "$title: ${nameArray[valueArray.indexOf(value)]}"
+            true
+        }
+
     }
 }
