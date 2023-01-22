fab3e507777694c4dbfe96516ba325b62688123b
demosss <spbdemosss@gmail.com>
2023-01-22 21:43:52 +0300

    lesson_15

--- app/src/main/java/com/spbdemosss/gpstrackercurse/utils/DialogManager.kt
+++ app/src/main/java/com/spbdemosss/gpstrackercurse/utils/DialogManager.kt
@@ -6,17 +6,21 @@ import android.widget.Toast
 import com.spbdemosss.gpstrackercurse.R
 
 object DialogManager {
-    fun showLocEnableDialog(context: Context){
+    fun showLocEnableDialog(context: Context, listener: Listener){
         val builder = AlertDialog.Builder(context)
         val dialog = builder.create()
         dialog.setTitle(R.string.location_disabled)
         dialog.setMessage(context.getString(R.string.location_dialog_message))
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes"){
-            _, _ -> Toast.makeText(context, "Yes", Toast.LENGTH_SHORT).show()
+            _, _ ->listener.onClick()
         }
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No"){
-                _, _ -> Toast.makeText(context, "No", Toast.LENGTH_SHORT).show()
+                _, _ -> dialog.dismiss()
         }
         dialog.show()
+    }
+
+    interface Listener{
+        fun onClick()
     }
 }
