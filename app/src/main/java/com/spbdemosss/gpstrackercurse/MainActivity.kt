61cad74e76d179656de16b123363f4132a3f56e1
demosss <spbdemosss@gmail.com>
2023-01-22 14:51:06 +0300

    Create project

---
+++ app/src/main/java/com/spbdemosss/gpstrackercurse/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.spbdemosss.gpstrackercurse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.spbdemosss.gpstrackercurse.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
