aeff22453b6493cced2fe70a71d5dc51272c1f40
demosss <spbdemosss@gmail.com>
2023-01-24 22:25:20 +0300

    lesson_21

---
+++ app/src/main/java/com/spbdemosss/gpstrackercurse/utils/TimeUtils.kt
@@ -0,0 +1,17 @@
+package com.spbdemosss.gpstrackercurse.utils
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+object TimeUtils {
+    @SuppressLint("SimpleDateFormat")
+    private val timeFormatter = SimpleDateFormat("HH:mm:ss")
+
+    fun getTime(timeInMillis: Long): String {
+        val cv = Calendar.getInstance()
+        timeFormatter.timeZone = TimeZone.getTimeZone("UTC")
+        cv.timeInMillis = timeInMillis
+        return timeFormatter.format(cv.time)
+    }
+}
