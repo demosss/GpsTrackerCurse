a29a97ae022f9b186d0d4d6e01faca4a8e0d843c
demosss <spbdemosss@gmail.com>
2023-02-12 13:32:09 +0300

    lesson_32

--- app/src/main/java/com/spbdemosss/gpstrackercurse/fragments/MainFragment.kt
+++ app/src/main/java/com/spbdemosss/gpstrackercurse/fragments/MainFragment.kt
@@ -132,6 +132,11 @@ class MainFragment : Fragment() {
             startLocService()
         } else {
             timer?.cancel()
+            DialogManager.showSaveDialog(requireContext(), object : DialogManager.Listener{
+                override fun onClick() {
+                    showToast("Track saved!!!")
+                }
+            })
             activity?.stopService(Intent(activity, LocationService::class.java))
             binding.fStartStop.setImageResource(R.drawable.ic_play)
 
--- app/src/main/java/com/spbdemosss/gpstrackercurse/utils/DialogManager.kt
+++ app/src/main/java/com/spbdemosss/gpstrackercurse/utils/DialogManager.kt
@@ -2,8 +2,12 @@ package com.spbdemosss.gpstrackercurse.utils
 
 import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
 import android.widget.Toast
 import com.spbdemosss.gpstrackercurse.R
+import com.spbdemosss.gpstrackercurse.databinding.SaveDialogBinding
 
 object DialogManager {
     fun showLocEnableDialog(context: Context, listener: Listener){
@@ -17,6 +21,22 @@ object DialogManager {
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No"){
                 _, _ -> dialog.dismiss()
         }
+        dialog.show()
+    }
+
+    fun showSaveDialog(context: Context, listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val binding = SaveDialogBinding.inflate(LayoutInflater.from(context), null, false)
+        builder.setView(binding.root)
+        val dialog = builder.create()
+        binding.bSave.setOnClickListener {
+            listener.onClick()
+            dialog.dismiss()
+        }
+        binding.bCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.show()
     }
 
